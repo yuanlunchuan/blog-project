7a5e9a6a62c855f848189cafd74e1ea5b574a914
Yuan Lunchuan <799652013@qq.com>
2016-08-06 20:29:58 +0800

    1, Add admin page.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb',
+        'USER': 'yuanlunchuan',
+        'PASSWORD': '5yuanlunchuan',
     }
 }
 
@@ -123,3 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+AUTH_USER_MODEL = 'blog.User'
