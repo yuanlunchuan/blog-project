210c184c7936851991fac278f84cddacd91dcd96
Yuan Lunchuan <799652013@qq.com>
2016-08-07 23:18:44 +0800

    1, Add admin config.

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 
 from .models import *
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'desc', 'content',)
+    list_display_links = ('title', 'desc', 'content',)
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'desc', 'content', )
+        }),
+        ('高级设置', {
+            'classes': ('collapse',),
+            'fields': ('click_count', 'is_recommend', )
+        }),
+    )
+
 admin.site.register(User)
 admin.site.register(Tag)
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category)
 admin.site.register(Comment)
 admin.site.register(Links)
-
+admin.site.register(Ad)
