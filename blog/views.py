7a523a83ff5909108dec5308223e9e79be9f6033
Yuan Lunchuan <799652013@qq.com>
2016-08-17 00:38:36 +0800

    1, Add category.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
-    return render(request, 'index.html', locals())
+    try:
+        category_list = Category.objects.all()
+    except Exception as e:
+        print(e)
+    return render(request, 'index.html', { 'category_list': category_list })
