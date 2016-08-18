323a53b4687d8962a805fef69a402f72e51ee63c
Yuan Lunchuan <799652013@qq.com>
2016-08-18 23:54:32 +0800

    1, Add paginate.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, InvalidPage, EmptyPage, PageNotAnInteger
 from .models import *
 
 def index(request):
     try:
         category_list = Category.objects.all()
         articles = Article.objects.all()
+        paginator = Paginator(articles, 2)
+        try:
+            page = 1
+            if request.GET.get('page'):
+                page = int(request.GET.get('page'))
+            articles = paginator.page(page)
+        except (EmptyPage, InvalidPage, PageNotAnInteger):
+            articles = paginator.page(1)
     except Exception as e:
         print(e)
-    return render(request, 'index.html', { 'category_list': category_list , 'articles': articles })
+    return render(request, 'index.html', locals())
