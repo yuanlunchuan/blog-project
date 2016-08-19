a01ff13a132a3296804b4bafe9d3a5b3dbe53cee
Yuan Lunchuan <799652013@qq.com>
2016-08-19 22:04:14 +0800

    1, Add upload picture.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 AUTH_USER_MODEL = 'blog.User'
