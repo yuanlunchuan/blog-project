06d0e52ffd248297b919e9f52f838ed9f824812f
Yuan Lunchuan <799652013@qq.com>
2016-08-22 21:03:30 +0800

    1, Add log config.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
+import logging
 from django.shortcuts import render
+from django.conf import settings
 from django.core.paginator import Paginator, InvalidPage, EmptyPage, PageNotAnInteger
 from .models import *
+
+logger = logging.getLogger('blog.views')
+
+def global_setting(request):
+    return {
+        'SITE_NAME': settings.SITE_NAME,
+        'SITE_DESC': settings.SITE_DESC
+    }
 
 def index(request):
     try:
