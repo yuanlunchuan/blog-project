73baae35053c6793764e38914ec89f6bf4f7f2f4
Yuan Lunchuan <799652013@qq.com>
2016-08-22 23:39:17 +0800

    1, refactor link code.

--- blog/views.py
+++ blog/views.py
@@ -7,19 +7,24 @@ from .models import *
 logger = logging.getLogger('blog.views')
 
 def global_setting(request):
+    category_list = Category.objects.all()
+    ads = Ad.objects.all()
+    tags = Tag.objects.all()
+    archive_list = Article.objects.distinct_date()
+
     return {
+        'category_list': category_list,
+        'ads': ads,
+        'tags': tags,
+        'archive_list': archive_list,
         'SITE_NAME': settings.SITE_NAME,
         'SITE_DESC': settings.SITE_DESC
     }
 
 def index(request):
     try:
-        category_list = Category.objects.all()
         articles = Article.objects.all()
         paginator = Paginator(articles, 2)
-        ads = Ad.objects.all()
-        tags = Tag.objects.all()
-        archive_list = Article.objects.distinct_date()
 
         try:
             page = int(request.GET.get('page', 1))
@@ -31,17 +36,17 @@ def index(request):
     return render(request, 'index.html', locals())
 
 def archive(request):
-  try:
-    year = request.GET.get('year', None)
-    month = request.GET.get('month', None)
-    articles = Article.objects.filter(date_publish__contains=year+'-'+month)
-    paginator = Paginator(articles, 2)
     try:
-      page = int(request.GET.get('page', 1))
-      articles = paginator.page(page)
-    except (EmptyPage, InvalidPage, PageNotAnInteger):
-      articles = paginator.page(1)
+        year = request.GET.get('year', None)
+        month = request.GET.get('month', None)
+        articles = Article.objects.filter(date_publish__contains=year+'-'+month)
+        paginator = Paginator(articles, 2)
+        try:
+            page = int(request.GET.get('page', 1))
+            articles = paginator.page(page)
+        except (EmptyPage, InvalidPage, PageNotAnInteger):
+            articles = paginator.page(1)
 
-  except Exception as e:
-    print(e)
-  return render(request, 'archive.html', locals())
+    except Exception as e:
+        print(e)
+    return render(request, 'archive.html', locals())
