849cc951d188ad7fc5b2f4dd425ee6876ff2d5e7
Yuan Lunchuan <799652013@qq.com>
2016-08-23 14:09:35 +0800

    1, Refacte views code.

--- blog/views.py
+++ blog/views.py
@@ -22,31 +22,21 @@ def global_setting(request):
     }
 
 def index(request):
-    try:
-        articles = Article.objects.all()
-        paginator = Paginator(articles, 2)
-
-        try:
-            page = int(request.GET.get('page', 1))
-            articles = paginator.page(page)
-        except (EmptyPage, InvalidPage, PageNotAnInteger):
-            articles = paginator.page(1)
-    except Exception as e:
-        print(e)
+    articles = getPage(request, Article.objects.all())
     return render(request, 'index.html', locals())
 
 def archive(request):
-    try:
-        year = request.GET.get('year', None)
-        month = request.GET.get('month', None)
-        articles = Article.objects.filter(date_publish__contains=year+'-'+month)
-        paginator = Paginator(articles, 2)
-        try:
-            page = int(request.GET.get('page', 1))
-            articles = paginator.page(page)
-        except (EmptyPage, InvalidPage, PageNotAnInteger):
-            articles = paginator.page(1)
+    year = request.GET.get('year', None)
+    month = request.GET.get('month', None)
+    articles = getPage(request, Article.objects.filter(date_publish__contains=year + '-' + month))
 
-    except Exception as e:
-        print(e)
     return render(request, 'archive.html', locals())
+
+def getPage(request, articles):
+    paginator = Paginator(articles, 2)
+    try:
+        page = int(request.GET.get('page', 1))
+        articles = paginator.page(page)
+    except (EmptyPage, InvalidPage, PageNotAnInteger):
+        articles = paginator.page(1)
+    return articles
