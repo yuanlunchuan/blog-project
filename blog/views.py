9b60c1fa5413ccf67ef5f70e95b5002e0ad7166d
Yuan Lunchuan <799652013@qq.com>
2016-08-25 21:15:36 +0800

    1, Add comment.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 import logging
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.paginator import Paginator, InvalidPage, EmptyPage, PageNotAnInteger
 from .models import *
 from django.db.models import Count
+from blog.forms import *
 
 logger = logging.getLogger('blog.views')
 
@@ -45,26 +46,42 @@ def article(request):
         except Article.DoesNotExist:
             return render(request, 'failure.html', {'reason': '没有找到对应的文章'})
 
-    #     # 评论表单
-    #     comment_form = CommentForm({'author': request.user.username,
-    #                                 'email': request.user.email,
-    #                                 'url': request.user.url,
-    #                                 'article': id} if request.user.is_authenticated() else{'article': id})
-    #     # 获取评论信息
-    #     comments = Comment.objects.filter(article=article).order_by('id')
-    #     comment_list = []
-    #     for comment in comments:
-    #         for item in comment_list:
-    #             if not hasattr(item, 'children_comment'):
-    #                 setattr(item, 'children_comment', [])
-    #             if comment.pid == item:
-    #                 item.children_comment.append(comment)
-    #                 break
-    #         if comment.pid is None:
-    #             comment_list.append(comment)
+        comment_form = CommentForm({'author': request.user.username,
+                                    'email': request.user.email,
+                                    'url': request.user.url,
+                                    'article': id} if request.user.is_authenticated() else{'article': id})
+
+        comments = Comment.objects.filter(article=article).order_by('id')
+        comment_list = []
+        for comment in comments:
+            for item in comment_list:
+                if not hasattr(item, 'children_comment'):
+                    setattr(item, 'children_comment', [])
+                if comment.pid == item:
+                    item.children_comment.append(comment)
+                    break
+            if comment.pid is None:
+                comment_list.append(comment)
     except Exception as e:
       logger.error(e)
     return render(request, 'article.html', locals())
+
+def comment_post(request):
+    try:
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = Comment.objects.create(username=comment_form.cleaned_data["author"],
+                                             email=comment_form.cleaned_data["email"],
+                                             url=comment_form.cleaned_data["url"],
+                                             content=comment_form.cleaned_data["comment"],
+                                             article_id=comment_form.cleaned_data["article"],
+                                             user=request.user if request.user.is_authenticated() else None)
+            comment.save()
+        else:
+            return render(request, 'failure.html', {'reason': comment_form.errors})
+    except Exception as e:
+        logger.error(e)
+    return redirect(request.META['HTTP_REFERER'])
 
 def getPage(request, articles):
     paginator = Paginator(articles, 2)
